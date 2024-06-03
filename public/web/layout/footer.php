de0f0449aeafdce30e22fef9773b8e9220b20606
PatelTarachand <ptltarachand@gmail.com>
2024-06-03 17:37:31 +0530

    init snc project in laravel

---
+++ public/web/layout/footer.php
@@ -0,0 +1,142 @@
+             <!-- newslater-area -->
+             <section class="newslater-area pt-60 pb-60" style="background-color: #125875;">
+                <div class="container" >
+                    <div class="row align-items-center">
+                        <div class="col-xl-12 col-lg-12">
+                            <div class="section-title newslater-title">
+                                <div class="icon">
+                                    <img src="images/sncraipur.jpeg" alt="img">
+                                </div>                                
+                            </div>
+                        </div>
+                       
+                    </div>
+                   
+                </div>
+            </section>
+            <!-- newslater-aread-end -->
+         
+        </main>
+        <!-- main-area-end -->
+        <!-- footer -->
+        <footer class="footer-bg footer-p pt-90" style="background-color: #125875; background-image: url(img/bg/footer-bg.png);">
+            <div class="footer-top pb-70">
+                <div class="container">
+                    <div class="row justify-content-between">
+                        
+                        <div class="col-xl-4 col-lg-4 col-sm-6">
+                            <div class="footer-widget mb-30">
+                                <div class="f-widget-title">
+                                    <h2>About Us</h2>
+                                </div>
+                                <div class="f-contact">
+                                     <p>Shantiniketan College was established om 2000-01, in the college was promoted by Late Shri Ramlal Gupta Merorial Education Society with The motto to serve needy and poor people.</p>
+                                    
+                                    </div>
+                                 <div class="footer-social mt-10">                                    
+                                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                    <a href="#"><i class="fab fa-instagram"></i></a>
+                                    <a href="#"><i class="fab fa-twitter"></i></a>
+                                </div>    
+                            </div>
+                        </div>
+						<div class="col-xl-4 col-lg-4 col-sm-6">
+                            <div class="footer-widget mb-30">
+                                <div class="f-widget-title">
+                                    <h2>Our Links</h2>
+                                </div>
+                                <div class="footer-link">
+                                    <ul>                                        
+                                        <li><a href="index.php">Home</a></li>
+                                        <li><a href="about.php"> About</a></li>
+                                        <!-- <li><a href="courses.html">Courses</a></li> -->
+                                        <li><a href="contact.php"> Contact Us</a></li>
+                                        <!-- <li><a href="blog.html">Blog </a></li> -->
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                       
+                        <div class="col-xl-4 col-lg-4 col-sm-6">
+                            <div class="footer-widget mb-30">
+                                <div class="f-widget-title">
+                                  <h2>Contact Us</h2>
+                                </div>
+                                <div class="f-contact">
+                                    <ul>
+                                    <li>
+                                        <i class="icon fal fa-phone"></i>
+                                        
+                                        <span><a href="tel:0771- 2243085">0771- 2243085</a><br>
+                                        <a  href="tel:+919425514719">+91 94255 14719</a><br>
+                                           
+                                           
+                                        </span>
+                                        <span>
+                                        <a style="margin-left:54px;" href="tel:+919425526891">+91 94255 26891</a><br>
+                                        <a style="margin-left:54px;" href="tel:+917828168418">+91 78281 68418</a><br>
+                                        <a style="margin-left:54px;" href="tel:+917052101786">+91 88273 76688</a></br>
+                                        <a style="margin-left:54px;" href="tel:+918982880267">+91 89828 80267</a><br>
+                                        </span>
+                                    </li>
+                                   <li><i class="icon fal fa-envelope"></i>
+                                        <span>
+                                            <a href="mailto:shantiniketan2009@yahoo.co.in">shantiniketan2009@yahoo.co.in</a>
+                                        </span>
+                                    </li>
+                                    <li>
+                                        <i class="icon fal fa-map-marker-check"></i>
+                                        <span>Ring Road No.1, Near Pani Tanki,<br> Changorbhatha, Raipur, Chhattisgarh, 492001 </span>
+                                    </li>
+                                    
+                                </ul>
+                                    
+                                    </div>
+                                
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="copyright-wrap">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-lg-4">
+                          <div class="copy-text">
+                                <a href="index.php"><img src="images/snc.jpeg" alt="img" style="height:70px;width:70px;"></a>   
+                            </div>
+                        </div>
+                       
+                       <div class="col-lg-4 text-right text-xl-right">
+                           Copyright &copy; Shanatiniketan College 2023 . All rights reserved. </br> Developed By <a href="https://itmingo.com/" target="_blank" >IT Mingo LLP</a>     
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- footer-end -->
+
+		<!-- JS here -->
+        <script src="js/vendor/modernizr-3.5.0.min.js"></script>
+        <script src="js/vendor/jquery-3.6.0.min.js"></script>
+        <script src="js/popper.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/slick.min.js"></script>
+        <script src="js/ajax-form.js"></script>
+        <script src="js/paroller.js"></script>
+        <script src="js/wow.min.js"></script>
+        <script src="js/js_isotope.pkgd.min.js"></script>
+        <script src="js/imagesloaded.min.js"></script>
+        <script src="js/parallax.min.js"></script>
+        <script src="js/jquery.waypoints.min.js"></script>
+        <script src="js/jquery.counterup.min.js"></script>
+        <script src="js/jquery.scrollUp.min.js"></script>
+        <script src="js/jquery.meanmenu.min.js"></script>
+        <script src="js/parallax-scroll.js"></script>
+        <script src="js/jquery.magnific-popup.min.js"></script>
+        <script src="js/element-in-view.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+
+</html>
---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+ <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>SNC-Shanatiniketan College,Raipur,Chhattisgarh</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="shortcut icon" type="images/snc.jpeg" href="images/snc.jpeg">
+        <!-- Place favicon.ico in the root directory -->
+
+		<!-- CSS here -->
+        <link rel="stylesheet" href="{{ asset('web/css//bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('web/css//animate.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('web/css//magnific-popup.css') }}">
+        <link rel="stylesheet" href="{{ asset('web/fontawesome/css/all.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('web/css//dripicons.css') }}">
+        <link rel="stylesheet" href="{{ asset('web/css//slick.css') }}">
+        <link rel="stylesheet" href="{{ asset('web/css//meanmenu.css') }}">
+        <link rel="stylesheet" href="{{ asset('web/css//default.css') }}">
+        <link rel="stylesheet" href="{{ asset('web/css//style.css') }}">
+        <link rel="stylesheet" href="{{ asset('web/css//responsive.css') }}">
+    </head>
+@include('layout.menu')
+@yield('body')
+
+@include('layout.footer')
