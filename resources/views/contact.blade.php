62594eb5bf4a9ddeedd20fc8e3f1dfc4114772ee
PatelTarachand <ptltarachand@gmail.com>
2024-06-03 19:50:35 +0530

    add contact page

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,210 @@
+<?php include_once('layout/header.php'); ?>
+
+    <body>
+       <?php include_once('layout/menu.php'); ?>
+        <!-- main-area -->
+        <main>
+            
+           <!-- breadcrumb-area -->
+            <section class="breadcrumb-area d-flex  p-relative align-items-center" style="background-image:url(img/bg/bdrc-bg.png)">
+              
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-xl-12 col-lg-12">
+                            <div class="breadcrumb-wrap text-left">
+                                <div class="breadcrumb-title">
+                                    <h2>Contact Us</h2>  
+                                </div>
+                            </div>
+                        </div>
+                        <div class="breadcrumb-wrap2">
+                              
+                                <nav aria-label="breadcrumb">
+                                    <ol class="breadcrumb">
+                                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                                        <li class="breadcrumb-item active" aria-current="page">Contact Us</li>
+                                    </ol>
+                                </nav>
+                            </div>
+                        
+                    </div>
+                </div>
+            </section>
+            <!-- breadcrumb-area-end -->
+            
+           <!-- services-area -->          
+            <section id="services" class="services-area pt-120 pb-90 fix">
+                <div class="container">
+                   <div class="row">
+                         <div class="col-lg-12">
+                            <div class="section-title text-center mb-50 wow fadeInDown animated" data-animation="fadeInDown" data-delay=".4s">
+                                <h4><i class="fal fa-graduation-cap"></i> Contact Info</h4>
+                                <h2>
+                                 Get In Touch
+                                </h2>
+                             
+                            </div>
+                           
+                        </div>
+                    </div>
+                    <div class="row">
+                         <div class="col-lg-4 col-md-4">
+                             
+                          <div class="services-box text-center">
+                              <div class="services-icon">
+                                   <img src="img/bg/contact-icon01.png" alt="image">
+                                </div>
+                               <div class="services-content2">
+                                    <h6><a href="tel:07712243085">0771- 2243085</a></h6>  
+                                    <h6><a href="tel:+919425514719">+91 94255 14719</a></h6> 
+                                    <h6><a href="tel:+919425526891">+91 94255 26891</a></h6>  
+                                    <h6><a href="tel:+917828168418">+91 78281 68418</a></h6> 
+                                    <h6><a href="tel:+918827376688">+91 88273 76688</a></h6>  
+                                    <h6><a href="tel:+918982880267">+91 89828 80267</a></h6> 
+                                    
+                                </div>
+                            </div>   
+                             
+                         
+                        </div>
+                        <div class="col-lg-4 col-md-4">
+                             
+                          <div class="services-box text-center active">
+                              <div class="services-icon">
+                                  <img src="img/bg/contact-icon02.png" alt="image">
+                                </div>
+                               <div class="services-content2">
+                                    <h4><a href="mailto:shantiniketan2009@yahoo.co.in">shantiniketan2009@yahoo.co.in</a></h4>   
+                                    </br>
+                                     
+                                </div>
+                            </div>   
+                             
+                         
+                        </div>
+                        <div class="col-lg-4 col-md-4">
+                             
+                          <div class="services-box text-center">
+                              <div class="services-icon">
+                                 <img src="img/bg/contact-icon03.png" alt="image">
+                                </div>
+                               <div class="services-content2">
+                                    <h6>Ring Road No.1, Near Pani Tanki,</h6>   
+                                    <h6>Changorbhatha, Raipur, Chhattisgarh, 492001</h6>
+                                </div>
+                            </div>   
+                             
+                         
+                        </div>
+                        
+                    </div>
+                </div>
+            </section>
+            <!-- services-area-end -->
+            <!-- map-area-end -->
+            <div class="map fix" style="background: #f5f5f5;">
+                <div class="container-flud">
+                    
+                    <div class="row">
+                        <div class="col-lg-12">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d588.9260633231734!2d81.60450734195265!3d21.226876662075753!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a28ddf5002b4e35%3A0x2a57160a86a5316b!2sShantiniketan%20public%20school%20and%20College!5e0!3m2!1sen!2sin!4v1717058364275!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>   
+                    </div>
+                    </div>
+                </div>
+            </div>
+		     <!-- map-area-end -->
+             <!-- contact-area -->
+            <section id="contact" class="contact-area after-none contact-bg pt-120 pb-120 p-relative fix" style="background: #e7f0f8;">
+                
+                <div class="container">
+             
+					<div class="row">
+						
+                         
+                        <div class="col-lg-12 order-2">
+                            <div class="contact-bg">
+                            <div class="section-title center-align text-center mb-50">
+                                <h2>
+                                   Contact Form
+                                </h2>
+                               
+                            </div>
+                             
+						<form action="#" method="post" class="contact-form mt-30 text-center">
+							<div class="row">
+                            <div class="col-lg-4">
+                                <div class="contact-field p-relative c-name mb-30">                                    
+                                    <input type="text" id="firstn" name="firstn" placeholder="First Name" required>
+                                    <i class="icon fal fa-user"></i>
+                                </div>                               
+                            </div>
+							<div class="col-lg-4">                               
+                                <div class="contact-field p-relative c-subject mb-30">                                   
+                                    <input type="text" id="email" name="email" placeholder="Email" required>
+                                    <i class="icon fal fa-envelope"></i>
+                                </div>
+                            </div>		
+                            <div class="col-lg-4">                               
+                                <div class="contact-field p-relative c-subject mb-30">                                   
+                                    <input type="text" id="phone" name="phone" placeholder="Phone No." required>
+                                     <i class="icon fal fa-phone"></i>
+                                </div>
+                            </div>	                            
+                            <div class="col-lg-12">
+                                <div class="contact-field p-relative c-message mb-30">                                  
+                                    <textarea name="message" id="message" cols="30" rows="50" placeholder="Write comments"></textarea>
+                                     <i class="icon fal fa-edit"></i>
+                                </div>
+                                <div class="slider-btn  text-center">                                          
+                                            <button class="btn ss-btn" data-animation="fadeInRight" data-delay=".8s">Make An Request <i class="fal fa-long-arrow-right"></i></button>				
+                                        </div>                             
+                            </div>
+                            </div>
+                        
+                    </form>
+                            
+                            </div>
+                        
+						</div>
+					</div>
+                    
+                </div>
+               
+            </section>
+            <!-- contact-area-end -->
+       <!-- brand-area -->
+            <!-- <div class="brand-area pt-60 pb-60" style="background-color:#125875">
+                <div class="container">
+                    <div class="row brand-active">
+                        <div class="col-xl-2">
+                            <div class="single-brand">
+                                <img src="img/brand/b-logo1.png" alt="img">
+                            </div>
+                        </div>
+                        <div class="col-xl-2">
+                            <div class="single-brand">
+                                 <img src="img/brand/b-logo2.png" alt="img">
+                            </div>
+                        </div>
+                        <div class="col-xl-2">
+                            <div class="single-brand">
+                                 <img src="img/brand/b-logo3.png" alt="img">
+                            </div>
+                        </div>
+                        <div class="col-xl-2">
+                            <div class="single-brand">
+                                  <img src="img/brand/b-logo4.png" alt="img">
+                            </div>
+                        </div>
+                        <div class="col-xl-2">
+                            <div class="single-brand">
+                                 <img src="img/brand/b-logo5.png" alt="img">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> -->
+            <!-- brand-area-end -->        
+        </main>
+        <!-- main-area-end -->
+        <?php include_once('layout/footer.php'); ?>
